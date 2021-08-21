cc40bf614c7a923c4cbed3d782ced228ba230ddd
aironi <lzh360370285@qq.com>
2021-08-21 21:30:32 +0800

    rosalind

--- 07.Consensus and Profile.py
+++ 07.Consensus and Profile.py
@@ -4,58 +4,71 @@ Created on Sat Aug 21 13:31:15 2021
 
 @author: liuzihao
 """
+
 import re
 class Consensus:
+    #extracting sequence from fasta
+    """
+    All sequence should has same length
+    """
     @staticmethod
     def read_sequcence(fasta):
         seq = []
         for line in open(fasta):
-            if (re.search(r'^>',line) == None):
+            if re.search(r'^>',line) == None:
                 seq.append(line.strip().upper())
         return seq
 
     def __init__(self,fasta):
         self.sequence = Consensus.read_sequcence(fasta)
-        self.seq_list_length = len(self.sequence)
+        self.seq_nubmer = len(self.sequence)
         self.seq_length = len(self.sequence[0])
         self.base = ["A","C","G","T"]
     
     def get_consensus(self):
         seq_tmp = []
         seq3 = []
-        #提取各个位置的碱基
+        #extracting base from sequence by index
         for base in range(self.seq_length):
-            for seq in range(self.seq_list_length):
+            for seq in range(self.seq_nubmer):
                 seq_tmp.append(self.sequence[seq][base])
 
-                if len(seq_tmp) == self.seq_list_length:
+                if len(seq_tmp) == self.seq_nubmer:
                     seq3.append(seq_tmp)
                     seq_tmp = []
-        
         del seq_tmp
-        #统计各个碱基出现的频次
-        count_dic = {}
+
+        # frequence of each base in seq3
+        count_dic_tmp = {}
         count_lst = []
         for con in seq3:
             for con_base in self.base:
-                count_dic[con_base] = con.count(con_base)
-                if len(count_dic) == len(self.base):
-                    count_lst.append(count_dic)
-                    count_dic = {}
-        del count_dic
-        #得到Consensus序列
+                count_dic_tmp[con_base] = con.count(con_base)
+                if len(count_dic_tmp) == len(self.base):
+                    count_lst.append(count_dic_tmp)
+                    count_dic_tmp = {}
+        del count_dic_tmp
+
+        #sequence of Consensus
         con_seq = ""
         for i in count_lst:
             res = max(i, key=lambda x: i[x])
             con_seq = con_seq + res
-        fd = open('result.txt','a')
-        fd_seq = open('Consensus.seq','a')
-        
-        fd_seq.write(">Consensus\n")
-        fd_seq.write(f"{con_seq}")
-        
+        #save sequence of Consensus
+        fd = open('result.txt','w')
         fd.write(f"{con_seq}\n")
-        #计数结果转换，并将结果写入文件
+        
+        fd_seq = open('Consensus.seq','w')
+        fd_seq.write(f">Consensus\n{con_seq}")
+        fd_seq.close()
+        
+        
+        #Convert data
+        """
+        A:	5	1	0	0	5	5	0	0
+        C:	0	0	1	4	2	0	6	1
+        ......
+        """
         count_restlt = {}
         lst_tmp = []
 
@@ -67,19 +80,18 @@ class Consensus:
                     count_restlt[base] = lst_tmp
                     lst_tmp = []
         del count_lst
-
+        
+        #save detail data (count table)
         for i in self.base:
             r1 = [str(b) for b in count_restlt[i]]
-            r1 =" ".join(r1)
+            r1 ="\t".join(r1)
             fd.write(f"{r1}\n")
         fd.close()
-        fd_seq.close()
+        
         return con_seq
 
-def main():
-    a = Consensus(fasta=".//data//07.test_file.fasta")
-    seq = a.get_consensus()
-    print(f"Consensus: {seq}")
 if __name__=='__main__':
-    main()
+     a = Consensus(fasta=".//data//07.test_file.fasta")
+     seq = a.get_consensus()
+     print(f"Consensus: {seq}")
 
