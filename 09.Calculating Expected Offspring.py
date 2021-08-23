61291d83515e361e1b061163421a5f89845311e3
aironi <lzh360370285@qq.com>
2021-08-24 00:25:51 +0800

    rosalind

---
+++ 09.Calculating Expected Offspring.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+所给：6个不超过20000的非负整数，分别对应于一个群体中在某基因位点上不同基因型组合的夫妻数目。六个整数对应的基因型分别如下：
+
+AA-AA
+
+AA-Aa
+
+AA-aa
+
+Aa-Aa
+
+Aa-aa
+
+aa-aa 
+需得：下一代表现显性性状的期望数，假设每对夫妻只有两个后代 
+作者：未琢 https://www.bilibili.com/read/cv2030564/?from=readlist 出处：bilibili
+"""
